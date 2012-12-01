4f08f160f1f615c7190597cfd61d77c299777134
Astrellon <sovereign250@gmail.com>
2012-12-01 21:33:32 +1100

    - Left out of previous commit.

---
+++ ui/ievent_listener.cpp
@@ -0,0 +1,46 @@
+#include "ievent_listener.h"
+
+#include "event_interface.h"
+
+namespace am {
+namespace ui {
+
+	IEventListener::~IEventListener()
+	{
+		ListeningList::iterator iter;
+		for (iter = mListeningTo.begin(); iter != mListeningTo.end(); ++iter)
+		{
+			(*iter)->removeEventListener(this);
+		}
+		mListeningTo.clear();
+	}
+
+	void IEventListener::addListeningTo(EventInterface *e)
+	{
+		if (e != NULL)
+		{
+			mListeningTo.push_back(e);
+		}
+	}
+	void IEventListener::removeListeningTo(EventInterface *e)
+	{
+		if (e != NULL)
+		{
+			ListeningList::const_iterator iter;
+			for (iter = mListeningTo.begin(); iter != mListeningTo.end(); ++iter)
+			{
+				if (*iter == e)
+				{
+					mListeningTo.erase(iter);
+					break;
+				}
+			}
+		}
+	}
+	const IEventListener::ListeningList &IEventListener::getListeningToList() const
+	{
+		return mListeningTo;
+	}
+
+}
+}
