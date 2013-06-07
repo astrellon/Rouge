14794e9ad97ae6fd12d4a3de03a5401a7c858507
Alan <sovereign250@gmail.com>
2013-06-08 08:56:13 +1000

    - Added test for Lua Sound wrapper.

---
+++ tests/test_lua_sound.cpp
@@ -0,0 +1,58 @@
+#include "test_lua_sound.h"
+
+#include <tests/asserts.h>
+
+#include <base/handle.h>
+using namespace am::base;
+
+#include <lua/lua_state.h>
+using namespace am::lua;
+
+#include <sfx/sfx_isound.h>
+#include <sfx/sfx_engine.h>
+using namespace am::sfx;
+
+#include <lua/wrappers/sfx/lua_sound.h>
+using namespace am::lua::sfx;
+
+extern "C" 
+{ 
+#	include <lua/src/lua.h>
+#	include <lua/src/lauxlib.h>
+#	include <lua/src/lualib.h>
+} 
+
+namespace am {
+namespace tests {
+
+	bool TestLuaSound::testSimple() {
+		LuaState lua;
+		
+		assert(lua.loadString("Sound = import(\"Sound\")\n"
+			//"testSound = Sound.new(\"data/
+			));
+
+		assert(lua.hasGlobalFunction("getName"));
+		lua_acall(lua, 0, 1);
+		am_equalsStr("testType", lua_tostring(lua, -1));
+		lua.pop(1);
+
+		assert(lua.hasGlobalFunction("getFullName"));
+		lua_acall(lua, 0, 1);
+		am_equalsStr("Test Type", lua_tostring(lua, -1));
+		lua.pop(1);
+
+		assert(lua.hasGlobalFunction("setFullName"));
+		lua.push("New Test Type");
+		lua_acall(lua, 1, 0);
+
+		assert(lua.hasGlobalFunction("getFullName"));
+		lua_acall(lua, 0, 1);
+		am_equalsStr("New Test Type", lua_tostring(lua, -1));
+		lua.pop(1);
+
+		return true;
+	}
+
+}
+}
---
+++ tests/test_lua_sound.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <base/imanaged.h>
+#include <base/handle.h>
+using namespace am::base;
+
+#include <tests/test_suite.h>
+
+extern "C"
+{
+#	include <lua/src/lstate.h>
+}
+
+namespace am {
+namespace tests {
+
+	class TestLuaSound : public TestSuite {
+	public:
+		TestLuaSound() {}
+		~TestLuaSound() {}
+	
+		virtual void runCases() {
+			runCase(testSimple);
+		}
+	
+		virtual const char *getSuiteName() const {
+			return "am::lua::sfx::TestLuaSound";
+		}
+
+	protected:
+
+		bool testSimple();
+
+	};
+
+}
+}
