c71390d92ccd3bfb9cc38541398df487863cef0e
astrellon <sovereign250@gmail.com>
2013-08-19 13:16:40 +1000

    - Some more files that needed to be added/removed.

--- data/assets/characters/mainChar/front.lua
+++
@@ -1,3 +0,0 @@
--- Front Main Character Sprite
-asset = {}
-asset.texture = "data/textures/characters/kaswe.png"
---
+++ data/assets/tiles/nature.lua
@@ -0,0 +1,37 @@
+do
+	-- Grass
+	am.gfx_engine.asset(am.asset.new("grass")
+		:add_texture("data/textures/tiles/grass_dirt_sand.png")
+		:texture_window_pixel({bottom_y = 64})
+		:num_frames(4, 2))
+		
+	-- Grass Transition
+	am.gfx_engine.asset(am.asset.new("grass_transition")
+		:add_texture("data/textures/tiles/grass_transition.png")
+		:num_frames(16, 2))
+		
+	-- Dirt
+	am.gfx_engine.asset(am.asset.new("dirt")
+		:add_texture("data/textures/tiles/grass_dirt_sand.png")
+		:texture_window_pixel({top_y = 64, right_x = 64})
+		:num_frames(2, 2))
+		
+	-- Swamp
+	am.gfx_engine.asset(am.asset.new("swamp")
+		:add_texture("data/textures/tiles/swamp.png")
+		:frame_rate(1)
+		:num_frames(2, 2))
+	
+	-- Water
+	am.gfx_engine.asset(am.asset.new("water")
+		:add_texture("data/textures/tiles/water.png")
+		:frame_rate(1)
+		:num_frames(2, 2))
+	
+	-- Bricks
+	am.gfx_engine.asset(am.asset.new("bricks")
+		:add_texture("data/textures/tiles/bricks.png")
+		:texture_window_pixel({right_x = 31, bottom_y = 31}))
+	
+	
+end
